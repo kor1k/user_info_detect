9bf31ba679a6f6504999e4d50e23c4915f33f9c8
kor1k <koortttt@gmail.com>
2019-09-03 17:02:42 +0300

    added view of taken data to user. added style and gitignore

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-$.getJSON('https://api.ipdata.co/?api-key=test',function(data){console.log(data)});
+$.getJSON('https://api.ipdata.co/?api-key=test', function (data){$('#user_ip_data').html(data.ip);$('#continent_name_data').html(data.continent_name);$('#country_name_data').html(data.country_name);$('#country_code_data').html(data.country_code);$('#flag').attr('src', data.flag);console.log(data)
+});
