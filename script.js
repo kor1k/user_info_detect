0cbe5be7a9e1d675b59d725c9ad272c3fd58901a
kor1k <koortttt@gmail.com>
2019-09-04 09:03:54 +0300

    cleaned up code and minified js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-$.getJSON('https://api.ipdata.co/?api-key=test', function (data){$('#user_ip_data').html(data.ip);$('#continent_name_data').html(data.continent_name);$('#country_name_data').html(data.country_name);$('#country_code_data').html(data.country_code);$('#flag').attr('src', data.flag);console.log(data)});
+$.getJSON("https://api.ipdata.co/?api-key=test",function(t){$("#user_ip_data").html(t.ip),$("#continent_name_data").html(t.continent_name),$("#country_name_data").html(t.country_name),$("#country_code_data").html(t.country_code),$("#flag").attr("src",t.flag),console.log(t)});
